a95617e75d55fedc3c4a9ef39a7344fde5c84f48
kjgamis <kjgamis@live.ca>
2023-04-17 15:30:30 -0400

    fix: does not update on PUT request

--- server/app.js
+++ server/app.js
@@ -7,16 +7,18 @@ const problemMiddleware = require('api-problem/lib/middleware')
 const errorHandlerMiddleware = require('./middleware/errorHandler')
 const messages = require('./routes/messages')
 
-app.use('/api/messages', messages)
-app.use('*', () => {
-  throw new Problem(404, 'Route does not exist')
-})
 app.use(cors())
 // parse form data
 app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
-app.use(problemMiddleware)
+
+app.use('/api/messages', messages)
+app.use('*', () => {
+  throw new Problem(404, 'Route does not exist')
+})
+
 app.use(errorHandlerMiddleware)
+app.use(problemMiddleware)
 
 module.exports = app
