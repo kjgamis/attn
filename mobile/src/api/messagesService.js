65913204af11ff25561e46f883e18f1004f7b091
kjgamis <kjgamis@live.ca>
2023-04-17 16:56:45 -0400

    feat: add crud operations with messages api and prod url

--- mobile/src/api/messagesService.js
+++ mobile/src/api/messagesService.js
@@ -1,6 +1,7 @@
-const url = 'http://localhost:8080'
+// const url = 'http://192.168.2.11:8000'
+const url = 'https://attn-api.ue.r.appspot.com'
 
-const getAllMessages = async () => {
+const fetchMessages = async () => {
   try {
     const response = await fetch(`${url}/api/messages`)
     const json = await response.json()
@@ -9,7 +10,8 @@ const getAllMessages = async () => {
     console.error(error)
   }
 }
-const createMessage = async (messageBody) => {
+
+const postMessage = async (messageBody) => {
   try {
     const response = await fetch(`${url}/api/messages`, {
       method: 'POST',
@@ -19,7 +21,37 @@ const createMessage = async (messageBody) => {
       body: JSON.stringify(messageBody)
     })
     const json = await response.json()
-    console.log(json)
+    return json.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const updateMessage = async ({ id, messageBody }) => {
+  try {
+    const response = await fetch(`${url}/api/messages/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageBody)
+    })
+    const json = await response.json()
+    return json.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const delMessage = async (messageId) => {
+  try {
+    const response = await fetch(`${url}/api/messages/${messageId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
     return json.data
   } catch (error) {
     console.error(error)
@@ -27,6 +59,8 @@ const createMessage = async (messageBody) => {
 }
 
 export {
-  getAllMessages,
-  // createMessage
+  fetchMessages,
+  postMessage,
+  updateMessage,
+  delMessage
 }
